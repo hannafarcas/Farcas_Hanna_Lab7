9ae4fdc052576e6544d1aba868818840832c1fe3
Hanna Farcas <hanna.farcas@gmail.com>
2023-12-01 22:48:58 +0200

    Laborator8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,28 @@
-﻿namespace Farcas_Hanna_Lab7;
+﻿using System;
+using Farcas_Hanna_Lab7.Data;
+using System.IO;
+
+
+namespace Farcas_Hanna_Lab7;
+
 
 public partial class App : Application
 {
+	static ShoppingListDatabase database;
+	public static ShoppingListDatabase Database
+	{
+		get
+		{
+			if (database == null)
+			{
+                database = new
+                ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                LocalApplicationData), "ShoppingList.db3"));
+            }
+			return database;
+		}
+	}
+
 	public App()
 	{
 		InitializeComponent();
