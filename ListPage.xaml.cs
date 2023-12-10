aa940088328611fad471b562779724048e51a46a
Hanna Farcas <hanna.farcas@gmail.com>
2023-12-10 23:18:22 +0200

    Laborator9

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -9,6 +9,7 @@ public partial class ListPage : ContentPage
 		InitializeComponent();
 	}
 
+
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -23,4 +24,22 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
